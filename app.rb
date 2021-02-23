28ba07d915109f55ed7758ba183a09220328e146
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-02-22 21:08:18 -0500

    commit tree 2cebd97ad7415a19c9b41ddd8e4aebd03fc1f8e6

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end 
+
+    get '/name' do
+        "My name is Jaclyn"
+    end 
+
+    get '/hometown' do
+        "My hometown is Port Chester"
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is Natural by Shelly"
+    end 
+
 end
